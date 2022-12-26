3a62dc0b1fbd334fab31cae5095eadba8bd502bf
Jordi Mas <jmas@softcatala.org>
2022-12-26 21:18:22 +0100

    Fix init docker

--- transcribe-service/docker/entry-point.sh
+++ transcribe-service/docker/entry-point.sh
@@ -1,7 +1,5 @@
 UPLOAD_FOLDER=/srv/data/files/
-SAVED_TEXTS=/srv/data/saved/
 mkdir -p $UPLOAD_FOLDER
-mkdir -p $SAVED_TEXTS
 
 # The requests do not requiere CPU since they are waiting from CTranslate2, then threads is more appropiated
-gunicorn --workers=2 --graceful-timeout 90 --timeout 90 --threads=4 translate-service:app -b 0.0.0.0:8700
+gunicorn --workers=2 --graceful-timeout 90 --timeout 90 --threads=4 transcribe-service:app -b 0.0.0.0:8700
