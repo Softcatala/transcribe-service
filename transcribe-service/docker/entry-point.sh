a58d4016375d6f010d1b73b440bf5952251d8acc
Jordi Mas <jmas@softcatala.org>
2023-01-12 21:26:03 +0100

    Remove GPU comment

--- transcribe-service/docker/entry-point.sh
+++ transcribe-service/docker/entry-point.sh
@@ -1,5 +1,4 @@
 UPLOAD_FOLDER=/srv/data/files/
 mkdir -p $UPLOAD_FOLDER
 
-# The requests do not requiere CPU since they are waiting from CTranslate2, then threads is more appropiated
 gunicorn --workers=2 --graceful-timeout 90 --timeout 90 --threads=4 transcribe-service:app -b 0.0.0.0:8700
