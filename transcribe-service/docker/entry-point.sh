60abb23ec533a0a60e2c7d5648417532ec0bfecf
Jordi Mas <jmas@softcatala.org>
2023-03-11 11:05:37 +0100

    2 workers only

--- transcribe-service/docker/entry-point.sh
+++ transcribe-service/docker/entry-point.sh
@@ -1,4 +1,4 @@
 UPLOAD_FOLDER=/srv/data/files/
 mkdir -p $UPLOAD_FOLDER
 
-gunicorn --workers=2 --graceful-timeout 90 --timeout 90 --threads=4 transcribe-service:app -b 0.0.0.0:8700
+gunicorn --workers=2 --graceful-timeout 90 --timeout 90 transcribe-service:app -b 0.0.0.0:8700
