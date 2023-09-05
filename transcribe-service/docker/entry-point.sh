4ff3d5dc95f03338b98746a8f6be2c028832cc8f
Jordi Mas <jmas@softcatala.org>
2023-09-05 07:05:07 +0200

    Enable upload of larger files

--- transcribe-service/docker/entry-point.sh
+++ transcribe-service/docker/entry-point.sh
@@ -1,4 +1,5 @@
 UPLOAD_FOLDER=/srv/data/files/
 mkdir -p $UPLOAD_FOLDER
 
-gunicorn --workers=2 --graceful-timeout 90 --timeout 90 transcribe-service:app -b 0.0.0.0:8700
+# High timeout to allow upload using POST of large video/audio files
+gunicorn --workers=2 --graceful-timeout 600 --timeout 600 transcribe-service:app -b 0.0.0.0:8700
