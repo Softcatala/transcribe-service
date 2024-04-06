a2defbcdad6ea6741cc0d041c605eaa50acc6cdb
Jordi Mas <jmas@softcatala.org>
2024-04-06 19:30:51 +0200

    Remove similar users checking

--- transcribe-batch/batchfilesdb.py
+++ transcribe-batch/batchfilesdb.py
@@ -22,7 +22,6 @@ import uuid
 import fnmatch
 import logging
 from typing import Optional
-from similarusers import SimilarUsers
 
 class BatchFile():
     def __init__(self,
@@ -128,12 +127,6 @@ class BatchFilesDB(Queue):
 
         return records
         
-    def get_count_of_files_for_similar_users(self, email):
-        records = self.select()
-        email_list = list(map(lambda record: record.email, records))
-        similar_users = SimilarUsers()
-        return similar_users.get_count_of_files_for_similar_users(email_list, email)
-
     def _read_record_from_uuid(self, _uuid):
         record_fullpath = os.path.join(self.ENTRIES, _uuid + ".dbrecord")
         record = self._read_record(record_fullpath)
