cb9e393831efca8ff579a7db7221f3fafeba3cdd
NoOPeEKS <arnauapps@gmail.com>
2026-05-10 17:46:14 +0200

    fix: add typehints to utils

--- transcribe-service/src/transcribe_service/utils.py
+++ transcribe-service/src/transcribe_service/utils.py
@@ -1,20 +1,20 @@
 import unicodedata
 from urllib.parse import quote
 
-from transcribe_core.batchfilesdb import BatchFilesDB
+from transcribe_core.batchfilesdb import BatchFile, BatchFilesDB
 from transcribe_core.processedfiles import ProcessedFiles
 
 from transcribe_service.constants import ALLOWED_MIMEYPES
 
 
-def _get_record(_uuid):
+def _get_record(_uuid: str) -> BatchFile | None:
     processed_dir = ProcessedFiles.get_processed_directory()
     db = BatchFilesDB(processed_dir)
     return db._read_record_from_uuid(_uuid)
 
 
 # Reference: https://github.com/pallets/werkzeug/blob/main/src/werkzeug/utils.py#L454
-def _get_download_names(download_name, ext):
+def _get_download_names(download_name: str, ext: str) -> str:
     simple = unicodedata.normalize("NFKD", download_name)
     simple = simple.encode("ascii", "ignore").decode("ascii")
     # safe = RFC 5987 attr-char
@@ -23,7 +23,7 @@ def _get_download_names(download_name, ext):
     return names
 
 
-def _get_mimetype(extension):
+def _get_mimetype(extension: str) -> str:
     mimetype = ALLOWED_MIMEYPES.get(extension)
 
     if not mimetype:
@@ -36,8 +36,9 @@ def _get_mimetype(extension):
 
     return mimetype
 
-def _allowed_file(filename):
+
+def _allowed_file(filename: str) -> bool:
     return (
         "." in filename
-        and filename.rsplit(".", 1)[1].lower() in ALLOWED_MIMEYPES.keys()
+        and filename.rsplit(".", 1)[1].lower() in ALLOWED_MIMEYPES
     )
