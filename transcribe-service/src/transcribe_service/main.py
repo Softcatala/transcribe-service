e188451538bc2b10607d50a32964c281de306494
NoOPeEKS <arnauapps@gmail.com>
2026-06-23 00:02:09 +0200

    fix(cors): add cors middleware

--- transcribe-service/src/transcribe_service/main.py
+++ transcribe-service/src/transcribe_service/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from transcribe_service.logging import init_logging
 from transcribe_service.routes import file, stats, uuid
@@ -24,6 +25,14 @@ app.include_router(legacy_uuid.get_uuid_router)
 app.include_router(legacy_uuid.delete_uuid_router)
 app.include_router(legacy_file.get_file_router)
 app.include_router(legacy_file.upload_file_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_headers=["*"],
+    allow_methods=["*"],
+    allow_credentials=True,
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8700)
